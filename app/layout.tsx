8f8535d6725801e2f6fe037a78e80adee505ecec
kmnxtbuck <karan@nxtbuck.com>
2025-03-27 14:08:55 -0400

    Profit orbit landing page (#13)
    * comment footer
    
    * landing-page
    
    * Update aws url
    
    * Added GA
    
    * Added robots.txt & metadata
    
    * Add Google tags
    
    * Fix compilation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,12 @@
 // These styles apply to every route in the application
 import Script from 'next/script';
 import './globals.css';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'NxtBuck | Home',
+    description: 'Track Your Ad Spend ROI in Real-Time',
+  };
 
 export default function RootLayout({ children } : { children: React.ReactNode }) {
     return (
