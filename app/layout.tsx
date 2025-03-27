245d6769b255c201bfd8ae91562c76bc01d0eebe
kmnxtbuck <karan@nxtbuck.com>
2025-03-26 21:09:14 -0400

    comment footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 // These styles apply to every route in the application
 import './globals.css'
-import Footer from '@/components/Footer/Footer'
 
 export default function RootLayout({ children } : { children: React.ReactNode }) {
     return (
@@ -8,9 +7,9 @@ export default function RootLayout({ children } : { children: React.ReactNode })
             <body className="h-full">
                 <div className="bg-[url(/bg-2xl.jpg)] 
                                 bg-cover bg-center 
-                                grayscale-75 -z-75 body flex flex-col h-full">
-                    <main className="flex-grow max-h-4/5">{children}</main>
-                    <footer className="max-h-1/5"><Footer /></footer> 
+                                grayscale-75 -z-75 body flex flex-col h-screen">
+                    <main className="flex-grow overflow-auto">{children}</main>
+                    {/*<footer><Footer /></footer>*/}
                 </div>
             </body>
         </html>
