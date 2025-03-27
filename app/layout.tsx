9e10273b1949ba1ea542f4eaa57a39848a1690b8
kmnxtbuck <karan@nxtbuck.com>
2025-03-27 00:02:12 -0400

    Added GA

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,25 @@
 // These styles apply to every route in the application
+import Script from 'next/script';
 import './globals.css'
 
 export default function RootLayout({ children } : { children: React.ReactNode }) {
     return (
         <html lang="en">
+            <head>
+                {/* GA Script */}
+                <Script
+                    src={`https://www.googletagmanager.com/gtag/js?id=G-75C7SVN9J5`}
+                    strategy="afterInteractive"
+                />
+                <Script id="ga-init" strategy="afterInteractive">
+                    {`
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', 'G-75C7SVN9J5');
+                    `}
+                </Script>
+            </head>
             <body>
                 <div className="bg-[url(/bg-2xl.jpg)] 
                                 bg-cover bg-center 
